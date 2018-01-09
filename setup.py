f42e54e1341c4051a8b7e27d53f7829219702ef6
b-hodges <brjhodges@gmail.com>
2018-01-09 15:16:56 -0600

    Add setup files

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='equations',
+      version='1.0.0',
+      description='Python extensible math parser for evaluating arbitrary expressions',
+      author='Brian Hodges',
+      url='https://github.com/b-hodges/equations',
+      packages=['equations'])
