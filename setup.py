6c19eab01b8a3d7f16d1a598cf391e8dc405d6b6
b-hodges <brjhodges@gmail.com>
2018-01-09 18:08:20 -0600

    Restructured setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ from distutils.core import setup
 setup(name='equations',
       version='1.0.0',
       description='Python extensible math parser for evaluating arbitrary expressions',
-      author='Brian Hodges',
       url='https://github.com/b-hodges/equations',
+      author='Brian Hodges',
+      license='MIT',
+      keywords='equations expressions math parsing solving',
+      install_requires=['six'],
+      python_requires='>=2.6, ~=3.3',
       packages=['equations'])
