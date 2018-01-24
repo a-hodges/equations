5fe936233f598f1a185982a5f3e4b814062a5af7
b-hodges <16946799+b-hodges@users.noreply.github.com>
2018-01-23 18:22:53 -0600

    Moved config into setup.cfg

--- setup.py
+++ setup.py
@@ -1,14 +1,5 @@
 #!/usr/bin/env python3
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(name='equations',
-      version='1.0.0',
-      description='Python extensible math parser for evaluating arbitrary expressions',
-      url='https://github.com/b-hodges/equations',
-      author='b-hodges',
-      author_email="me@example.com",
-      license='MIT',
-      keywords='equations expressions math parsing solving',
-      install_requires=['six ~= 1.11'],
-      packages=find_packages())
+setup()
